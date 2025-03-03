0d793a4e1101c5275c0011b6f17041c3e806df66
adilbishsaruultuya <adilbish.saruultuya@gmail.com>
2025-03-03 12:46:07 +0900

    delcheck11]

--- frontend/src/common/axios/index.ts
+++ frontend/src/common/axios/index.ts
@@ -1,20 +1,5 @@
 import axios from "axios";
 
-// export const api = axios.create({
-//   baseURL: "https://backtomongol.onrender.com",
-// });
-
-console.log("✅ API URL (axios):", process.env.NEXT_PUBLIC_API_URL); // ← ここで確認
-
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
-
-// import axios from "axios";
-
-// export const api = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_API_URL, // ✅ 環境変数を使う
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
